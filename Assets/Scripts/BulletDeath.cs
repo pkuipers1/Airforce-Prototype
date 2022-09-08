a88fcd3c8b6d1826a0ba458a199d0b215f13b6ee
Patricia Kuipers <pkpatriciakuipers@gmail.com>
2022-09-08 19:20:59 +0200

    Last fixes shooting feature

--- Assets/Scripts/BulletDeath.cs
+++ Assets/Scripts/BulletDeath.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class BulletDeath : MonoBehaviour
 {
     public GameObject impactEffect;
+    public float lifespan; 
     
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,12 @@ public class BulletDeath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        lifespan -= Time.deltaTime;
+
+        if (lifespan <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
