0772c72ff625503a9c750dd65dd7375889047ebb
bas-boop <dereusbas461@gmail.com>
2022-10-06 19:47:30 +0200

    Rotatie werkt, naast de negatief value's

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementPC : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     
@@ -21,7 +21,7 @@ public class PlayerMovementPC : MonoBehaviour
     [SerializeField] private float rotationThreshold;
 
     [Header("Booleans")]
-    [SerializeField] private bool keyPressed;
+    [SerializeField] private bool isMoving;
 
     private void Awake()
     {
@@ -30,9 +30,6 @@ public class PlayerMovementPC : MonoBehaviour
 
     private void Update()
     {
-        keyPressed = false;
-
-        RotatingPlayer();
         UpdatingAxis();
     }
 
@@ -40,26 +37,74 @@ public class PlayerMovementPC : MonoBehaviour
     {
         var rotationThresholdX = currentRotation.x < rotationThreshold && currentRotation.x > -rotationThreshold;
         var rotationThresholdZ = currentRotation.z < rotationThreshold && currentRotation.z > -rotationThreshold;
+        
+        if(rotationThresholdX) currentRotation.x += rotationSpeed.x;
 
-        if (rotationThresholdX) currentRotation.x += rotationSpeed.x;
-        if (rotationThresholdZ) currentRotation.z += rotationSpeed.z;
+        if(rotationThresholdZ) currentRotation.z += rotationSpeed.z;
 
-        if (!keyPressed) currentRotation *= 0.1f;
+        if (!isMoving) currentRotation = Vector3.zero;
 
-        if (currentRotation.x != 0) currentRotation.x *= 1;
-        if (currentRotation.z != 0) currentRotation.z *= 1;
+        if (horizontal != 0 && currentRotation.x != 0)
+        {
+            if(currentRotation.x > 0) 
+            {
+                currentRotation.x *= 0.5f;
+            }
+            else if(currentRotation.x < 0) 
+            {
+                currentRotation.x /= 0.5f;
+            }
+            Debug.Log("X: " + currentRotation.x);
+        }
 
-        transform.rotation = Quaternion.Euler(currentRotation.z, 0, currentRotation.x);
+        if (vertical != 0 && currentRotation.x != 0)
+        {
+            if(currentRotation.z > 0) 
+            {
+                currentRotation.z *= 0.5f;
+            }
+            else if(currentRotation.z < 0) 
+            {
+                currentRotation.z /= 0.5f;
+            }
+            Debug.Log("Z: " + currentRotation.z);
+        }
+
+        transform.rotation = Quaternion.Euler(currentRotation.x, 0, currentRotation.z);
+        
+        /*var rotationThresholdX = currentRotation.x < rotationThreshold && currentRotation.x > -rotationThreshold;
+        var rotationThresholdZ = currentRotation.z < rotationThreshold && currentRotation.z > -rotationThreshold;
+
+        if(!isMoving) return;
+        if (rotationThresholdX && horizontal != 0) currentRotation.x += rotationSpeed.x;
+        if (rotationThresholdZ && vertical != 0) currentRotation.z += rotationSpeed.z;
+
+        currentRotation.x *= rotationSpeed.x;
+        currentRotation.z *= rotationSpeed.z;
+
+        //if (!keyPressed) currentRotation *= 0.1f;
+
+        if (currentRotation.x != 0) currentRotation.x *= 0.88f;
+        if (currentRotation.z != 0) currentRotation.z *= 0.88f;
+
+        Debug.Log(currentRotation);
+        
+        //transform.Rotate(currentRotation);
+        Debug.Log(transform.rotation);*/
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(horizontal * playerSpeed.x,0, vertical * playerSpeed.z);
+        RotatingPlayer();
     }
 
     private void UpdatingAxis()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0 || vertical != 0) isMoving = true;
+        else isMoving = false;
     }
 }
